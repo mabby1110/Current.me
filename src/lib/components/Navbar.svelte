01c4d981ea8ae36487a48b422bca5fd291db45b5
mabby1110 <rznns.mabby@gmail.com>
2024-08-12 00:16:36 -0600

    Cambios varios, se creo lógica de ruteo

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,5 @@
+<header>
+    <a href="/">Inicio</a>
+    <a href="/work">Proyectos</a>
+    <a href="/about">Sobre mi</a>
+</header>
---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { currentUser, pb } from '$lib/pocketbase';
+</script>
+<h1>
+    Welcome to current.me
+</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
