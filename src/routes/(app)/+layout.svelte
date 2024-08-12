db7b4b4959162c001b7d7a38629594ab63e9b8d5
mabby1110 <rznns.mabby@gmail.com>
2024-08-12 00:37:36 -0600

    Se establecio estilo incial

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,5 +2,26 @@
     import Navbar from "$lib/components/Navbar.svelte";
 </script>
 
-<Navbar/>
-<slot/>
+<nav>
+    <Navbar/>
+</nav>
+<main>
+    <slot/>
+</main>
+
+<style>
+    main {
+        background-color: black;
+        height: 95vh;
+        width: 100%;
+        display: flex;
+    }
+    nav {
+        background-color: black;
+        height: 5vh;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
